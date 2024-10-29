c2cdef9bc5eb68d18466b556b7b26d5c37780720
kiraimae <tipkemam@LCB-06>
2024-10-29 14:37:00 -0400

    Requirements documentation

--- group_assignment08/mainPackage/main.py
+++ group_assignment08/mainPackage/main.py
@@ -1,4 +1,16 @@
+# Name: Alexis Tipkemper-Sparks / Kayla Wilson / Jared Rababy 
+# email: Tipkemam@mail.uc.edu / Wilso5ky@mail.uc.edu / rababyjd@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date:  10/31/2024 HALLOWEENIE
+# Course #/Section: IS4010
+# Semester/Year:  Fall 2024
+# Brief Description of the assignment: VS project in github repo
 
+# Brief Description of what this module does: Tortures us relentlessly by having us practice committing/pushing 
+# and pull requests through collaborating on Github 
+# Citations: Prof. Nicholson / Past assignments / StackOverflow / GeeksforGeeks
+# Anything else that's relevant: Nada
+# ***********************************
 #main.py
 
 from dogPackage.dog import Dog
