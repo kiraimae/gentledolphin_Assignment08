d40ed139ce3a2eb8e01dcc42398a37c7fea8a06e
Wilso5ky <kaylawelch132@gmail.com>
2024-10-29 13:57:21 -0400

    cat code

--- group_assignment08/catPackage/cat.py
+++ group_assignment08/catPackage/cat.py
@@ -1,3 +1,20 @@
 
 #cat.py
 
+class Cat:
+    def __init__(self, name="Uno"): # Constructor method
+        self.name = name
+        self.playful = False # Default playfulness
+    def set_playful(self, is_playful): #Setter method to change playful status
+        self.playful = is_playful
+    def get_playful(self):  #Getter method that returns playful status
+        return self.playful
+    def meow(self): #Prints message
+        print(f"{self.name} meows: Meow!")
+    def display(self): #Returns string to decsribe cat
+        playfulness_status = "wants to play" if self.playful else "is not in the mood to play"
+        return f"{self.name} the Cat {playfulness_status}"
+    def __str__(self):
+        return f"Cat(name={self.name}, playful={self.playful})"
+    def __repr__(self):
+        return f"Cat({self.name!r})"
